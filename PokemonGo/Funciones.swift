787a6bdf7b359767a9702abe92d89fc246171fff
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-11-30 18:55:37 -0500

    agreado imagenes o pokemons

---
+++ PokemonGo/Funciones.swift
@@ -0,0 +1,33 @@
+//
+//  Funciones.swift
+//  PokemonGo
+//
+//  Created by Guerra Jose on 30/11/18.
+//  Copyright © 2018 Tecsup. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+func agregarPokemon() {
+    crearPokemon(xnombre: "Mew", ximagenNombre: "mew")
+    crearPokemon(xnombre: "Meowth", ximagenNombre: "meowth")
+    crearPokemon(xnombre: "Abra", ximagenNombre: "abra")
+    crearPokemon(xnombre: "Bullbasaur", ximagenNombre: "bullbasaur")
+    crearPokemon(xnombre: "Dratini", ximagenNombre: "dratini")
+    crearPokemon(xnombre: "Eevee", ximagenNombre: "eevee")
+    crearPokemon(xnombre: "Mankey", ximagenNombre: "mankey")
+    crearPokemon(xnombre: "Pikachu", ximagenNombre: "pikachu-2")
+    crearPokemon(xnombre: "Rattata", ximagenNombre: "rattata")
+    crearPokemon(xnombre: "Snorlax", ximagenNombre: "snorlax")
+    crearPokemon(xnombre: "Venonat", ximagenNombre: "venonat")
+    (UIApplication.shared.delegate as! AppDelegate).saveContext()
+}
+
+func crearPokemon(xnombre:String,ximagenNombre:String){
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let pokemon = Pokemon(context:context)
+    pokemon.nombre = xnombre
+    pokemon.imagenNombre = ximagenNombre
+}
